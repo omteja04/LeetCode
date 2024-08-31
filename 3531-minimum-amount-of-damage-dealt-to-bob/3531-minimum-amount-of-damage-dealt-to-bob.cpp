5f1329b7c1bec019b36909809932d2329ad459bf
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2024-08-31 21:59:07 +0530

    Time: 197 ms (100%), Space: 146 MB (73.33%) - LeetHub

--- 3531-minimum-amount-of-damage-dealt-to-bob/3531-minimum-amount-of-damage-dealt-to-bob.cpp
+++ 3531-minimum-amount-of-damage-dealt-to-bob/3531-minimum-amount-of-damage-dealt-to-bob.cpp
@@ -3,21 +3,13 @@ public:
     long long minDamage(int power, vector<int>& damage, vector<int>& health) {
         int n = damage.size();
         vector<pair<int, int>> damageAndTime;
-
-        // Create pairs of (damage, time to kill)
         for (int i = 0; i < n; ++i) {
-            // Calculate time to kill the enemy
             int timeToKill = (health[i] + power - 1) / power;
             damageAndTime.push_back({damage[i], timeToKill});
         }
-
-        // Sort pairs by damage in descending order
         sort(damageAndTime.begin(), damageAndTime.end(), [](const pair<int,int> &a, const pair<int, int> &b) {
             return (double) a.first / a.second > (double) b.first / b.second;
         });
-
-        // Calculate the total damage
-
         long long totalDamage = 0;
         for (int i = 0; i < n; i++) {
             totalDamage += damageAndTime[i].first;
