8f67177e0540d4dd54662d4732e73f3642b371fb
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2024-08-31 21:56:46 +0530

    Time: 206 ms (100%), Space: 146.2 MB (73.33%) - LeetHub

--- 3531-minimum-amount-of-damage-dealt-to-bob/3531-minimum-amount-of-damage-dealt-to-bob.cpp
+++ 3531-minimum-amount-of-damage-dealt-to-bob/3531-minimum-amount-of-damage-dealt-to-bob.cpp
@@ -8,24 +8,32 @@ public:
 
     long long minDamage(int power, vector<int>& damage, vector<int>& health) {
         int n = damage.size();
-        vector<vector<int>> enemy;
-        for (int i = 0; i < n; i++) {
-            enemy.push_back({damage[i], health[i]});
+        vector<pair<int, int>> damageAndTime;
+
+        // Create pairs of (damage, time to kill)
+        for (int i = 0; i < n; ++i) {
+            // Calculate time to kill the enemy
+            int timeToKill = (health[i] + power - 1) / power;
+            damageAndTime.push_back({damage[i], timeToKill});
         }
-        sort(enemy.begin(), enemy.end(), [power](const vector<int>& a, const vector<int>& b) {
-            return custom(a, b, power);
+
+        // Sort pairs by damage in descending order
+        sort(damageAndTime.begin(), damageAndTime.end(), [](const pair<int,int> &a, const pair<int, int> &b) {
+            return (double) a.first / a.second > (double) b.first / b.second;
         });
 
-        long long total = 0;
-        long long cur = 0;
-        for (auto i : enemy) {
-            cur += i[0];
+        // Calculate the total damage
+
+        long long totalDamage = 0;
+        for (int i = 0; i < n; i++) {
+            totalDamage += damageAndTime[i].first;
         }
-        for (auto i : enemy) {
-            int d = (i[1] + power - 1) / power;
-            total += d * cur;
-            cur -= i[0];
+        long long ans = 0;
+        for (const auto& [dmg, timeToKill] : damageAndTime) {
+            ans += (long long)totalDamage * timeToKill;
+            totalDamage -= dmg;
+
         }
-        return total;
+        return ans;
     }
 };
