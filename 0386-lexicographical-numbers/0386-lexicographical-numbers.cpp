4945e0bef7fb5dba4c8d5572cda73a3a99f40e41
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2024-09-21 22:52:39 +0530

    Time: 507 ms (5.05%), Space: 15.8 MB (36.64%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> vect;
+        for(int i=1; i<=n; ++i) vect.push_back(i);
+        sort(vect.begin(), vect.end(), [](int left, int right){
+            return to_string(left)<to_string(right);
+        });
+        return vect;
+    }
+};
