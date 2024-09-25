a9e47a39ae52130a4c7bf0b5cd49efd9a9e69582
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2024-09-25 22:28:22 +0530

    Time: 664 ms (26.68%), Space: 704.2 MB (63.84%) - LeetHub

---
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,69 @@
+struct Node{
+    Node* links[26] = {nullptr};
+    int prefixCnt = 0;
+    bool containsKey(char ch) {
+        return links[ch - 'a'] != NULL;
+    }
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+    void increasePrefix() {
+        prefixCnt++;
+    }
+    int getPrefix() {
+        return prefixCnt;
+    }
+};
+class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    void insert(string &word) {
+        Node* node = root;
+        for(int i = 0; i < word.length(); i++) {
+            if(!node->containsKey(word[i])) {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+            node->increasePrefix();
+        }
+    }
+    int cntPrefixScore(string &word) {
+        int cnt = 0;
+        Node* node = root;
+        for(int i = 0; i < word.length(); i++) {
+            if(!node->containsKey(word[i])) {
+                break;
+            }
+            node = node->get(word[i]);
+            cnt += node->getPrefix();
+            
+        }
+        return cnt;
+    }
+
+};
+class Solution {
+public:
+    vector<int> sumPrefixScores(vector<string>& words) {
+        Trie trie;
+        for(auto &word: words) {
+            trie.insert(word);
+        }
+        vector<int>ans;
+        
+        for(string &word: words){    
+            ans.push_back(trie.cntPrefixScore(word));
+        }
+        return ans;
+
+
+
+    }
+};
