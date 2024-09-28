5794d337e360f7b876fa9f9d3aebd39ea6c7e758
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2024-09-28 10:34:41 +0530

    Time: 18 ms (27.94%), Space: 16.1 MB (41.38%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        set<string> st;
+        for(auto &word : wordDict) {
+            st.insert(word);
+        }
+        int n = s.size();
+        vector<bool>dp(n + 1, false);
+        dp[0] = true;
+        for(int i = 1; i <= n; i++) {
+            for(int j  = 0; j < i; j++) {
+                if(dp[j] == true) {
+                    string str = s.substr(j,  i - j);
+                    if(st.contains(str)) {
+                        dp[i] = true;
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+};
