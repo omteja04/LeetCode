8ef0a26bedbdcf59bfaf47c0270aa45c8f7ff4b8
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2024-10-13 14:26:56 +0530

    Time: 3 ms (37.73%), Space: 46.9 MB (79.5%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -18,7 +18,7 @@ class Solution {
         }
         for(int i = 0 ; i < n; i++) {
             for(int j = 0;  j < n; j++) {
-                if(i == j) continue;
+                // if(i == j) continue;
                 if(isConnected[i][j] == 1) {
                     graph.get(i).add(j);
                     graph.get(j).add(i);
