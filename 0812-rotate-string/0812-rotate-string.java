2044dc9bca0b5204c8551c781b5019c632fbe53a
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2024-11-03 11:38:15 +0530

    Time: 0 ms (100%), Space: 41.3 MB (51.17%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,10 +1,16 @@
 class Solution {
-    public boolean helper(String s, String goal, int n) {
-        if(s.equals(goal)) return true;
-        else if(n == s.length()) return false;
-        return helper(s.substring(1) + s.charAt(0), goal, n + 1);
-    }
+    // public boolean helper(String s, String goal, int n) {
+    //     if(s.equals(goal)) return true;
+    //     else if(n == s.length()) return false;
+    //     return helper(s.substring(1) + s.charAt(0), goal, n + 1);
+    // }
     public boolean rotateString(String s, String goal) {
-        return helper(s, goal, 0);
+        // return helper(s, goal, 0);
+        if(s.length()!=goal.length())
+        return false;
+        String str=s+s;
+        if(str.contains(goal))
+        return true;
+        return false;
     }
 }
