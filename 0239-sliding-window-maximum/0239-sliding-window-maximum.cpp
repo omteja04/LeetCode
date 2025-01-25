f49fb5fa470a20dab9ccfdaed7e316cb058b24bf
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2025-01-25 22:36:50 +0530

    Time: 30 ms (40.91%), Space: 139.2 MB (61.29%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
         vector<int> res;
-        set<pair<int, int>> st;
-        int element = INT_MAX;
-        for(int i = 0; i < k; i++) {
-            st.insert({nums[i], i});
-        }
-        res.push_back(st.rbegin() -> first);
-        for(int i = k ; i < nums.size(); i++) {
-            st.erase({nums[i - k], i - k});
-            st.insert({nums[i], i});
-            res.push_back(st.rbegin() -> first);
-        }
-        return res;
-        
+        // set<pair<int, int>> st;
+        // int element = INT_MAX;
+        // for(int i = 0; i < k; i++) {
+        //     st.insert({nums[i], i});
+        // }
+        // res.push_back(st.rbegin() -> first);
+        // for(int i = k ; i < nums.size(); i++) {
+        //     st.erase({nums[i - k], i - k});
+        //     st.insert({nums[i], i});
+        //     res.push_back(st.rbegin() -> first);
+        // }
 
+        std::deque<int> dq;
+        for (int i = 0; i < k; i++) {
+            while (!dq.empty() && nums[i] > nums[dq.back()]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        // std::cout << dq.front() + 1 << " ";
+        res.push_back(nums[dq.front()]);
+        for (int i = k; i < n; i++) {
+            if (i - k == dq.front()) {
+                dq.pop_front();
+            }
+            while (!dq.empty() && nums[i] > nums[dq.back()]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            res.push_back(nums[dq.front()]);
+            // std::cout << dq.front() + 1 << " ";
+        }
+            return res;
     }
 };
