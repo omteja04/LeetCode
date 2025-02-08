73cb12225e09361512157f69983527a2bf323be8
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2025-02-08 10:22:20 +0530

    Time: 13 ms (37.49%), Space: 15.9 MB (47.78%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,68 +1,15 @@
-struct Node {
-    
-    Node *links[26] = {nullptr};
-    bool flag = false;
-    bool containsKey(char ch) {
-        return links[ch - 'a'] != nullptr;
-    }
-    void put(char ch, Node *node) {
-        links[ch - 'a'] = node;
-    }
-    Node *get(char ch) {
-        return links[ch - 'a'];
-    }
-    void setEnd() {
-        flag = true;
-    }
-    bool isEnd() {
-        return flag;
-    }
-};
-class Trie {
-private:
-    Node *root;
-
-public:
-    Trie() {
-        root = new Node();
-    }
-    void insert(string &word) {
-        Node *node = root;
-        for (char &ch : word) {
-            if (!node->containsKey(ch)) {
-                node->put(ch, new Node());
-            }
-            node = node->get(ch);
-        }
-        node->setEnd();
-    }
-    bool search(string &word, int start, int end) {
-        Node *node = root;
-        for (int i = start; i < end; i++) {
-            if (!node->containsKey(word[i])) {
-                return false;
-            }
-            node = node->get(word[i]);
-        }
-        return node->isEnd();
-    }
-    
-};
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        Trie trie;
-         for(auto &word : wordDict) {
-            trie.insert(word);
-        }
-        int n = s.size();
-        vector<bool>dp(n + 1, false);
+        int n = s.length();
+        vector<bool> dp(n + 1, false);
         dp[0] = true;
-        for(int i = 1; i <= n; i++) {
-            for(int j  = 0; j < i; j++) {
-                if (dp[j] && trie.search(s, j, i)) {
+        unordered_set<string> st(wordDict.begin(), wordDict.end());
+        for(int i = 1; i <= s.size(); i++) {
+            for(int j = 0; j < i; j++) {
+                if(dp[j] && st.count(s.substr(j, i - j))) {
                     dp[i] = true;
-                    break; 
+                    break;
                 }
             }
         }
