0afb51ac466402dec105f458b2dade1533343469
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2025-02-10 18:14:31 +0530

    Time: 1 ms (67.37%), Space: 14.4 MB (41.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
- vector<vector<int>> ans;
-    // set<vector<int>> ans;
-    void search(int i, int n, vector<int>& candidates, int target, int currSum, vector<int>&subset) {
-        if (currSum > target) return; 
-
-        if(i >= n) {
+    vector<vector<int>> ans;
+    void solve(int idx, int currSum, int n, vector<int>& candidates, int target, vector<int>& subset) {
+        if(currSum > target) return;
+        if(idx >= n) {
             if(currSum == target) {
-                // sort(subset.begin(), subset.end());
                 ans.push_back(subset);
             }
             return;
         }
-        subset.push_back(candidates[i]);
-        currSum += candidates[i];
-
-        search(i, n, candidates, target, currSum, subset);
+        subset.push_back(candidates[idx]);
+        solve(idx, currSum + candidates[idx], n, candidates, target, subset);
         subset.pop_back();
-        currSum -= candidates[i];
-
-        while(i + 1 < candidates.size() && candidates[i] == candidates[i + 1]) {
-            i++;
-        }
-
-        search(i + 1, n, candidates, target, currSum, subset);
+        solve(idx + 1, currSum, n, candidates, target, subset);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> subset;
+        int n = candidates.size();
         sort(candidates.begin(), candidates.end());
-        search(0, candidates.size(), candidates, target, 0, subset);
-        
+        solve(0, 0, n, candidates, target, subset);
         return ans;
     }
-    
 };
