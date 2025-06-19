3bde485f1eed9447aebc4efff4bb8f4f059f86a0
Omteja Yallapragada <119728012+omteja04@users.noreply.github.com>
2025-06-19 10:12:43 +0530

    Time: 4 ms (98.35%), Space: 87.9 MB (6.8%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     int partitionArray(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int cnt = 0;
-        int i = 0;
-        while (i < nums.size()) {
-            i = upper_bound(nums.begin(), nums.end(), nums[i] + k) - nums.begin();
-            cnt++;
+        // sort(nums.begin(), nums.end());
+        // int cnt = 0;
+        // int i = 0;
+        // while (i < nums.size()) {
+        //     i = upper_bound(nums.begin(), nums.end(), nums[i] + k) - nums.begin();
+        //     cnt++;
+        // }
+        // return cnt;
+        bitset<100001> exists;
+        int mini = nums[0];
+        int maxi = nums[0];
+        for(auto &ele: nums) {
+            mini = min(mini, ele);
+            maxi = max(maxi, ele);
+            exists[ele] = true;
+        }
+        int start = mini;
+        int cnt = 1;
+        for(int ele = start; ele <= maxi; ele++) {
+            if(exists[ele]) {
+                if(ele - start > k) {
+                    cnt++;
+                    start = ele;
+                }
+            }
         }
         return cnt;
+
+        
     }
 };
